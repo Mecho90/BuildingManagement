b273dedd900830b0b5c0205066ec2483cb5bb180
Mecho90 <donev.m.p@gmail.com>
2025-11-24 10:09:21 +0200

    new fix migration

--- building_management/core/migrations/0014_workorder_kind.py
+++ building_management/core/migrations/0014_workorder_kind.py
@@ -1,17 +1,6 @@
 from django.db import migrations, models
 
 
-def set_kind_defaults(apps, schema_editor):
-    schema_editor.execute("UPDATE core_workorder SET kind = 'MAINTENANCE' WHERE kind IS NULL;")
-    if schema_editor.connection.vendor != "sqlite":
-        schema_editor.execute("ALTER TABLE core_workorder ALTER COLUMN kind SET DEFAULT 'MAINTENANCE';")
-
-
-def unset_kind_defaults(apps, schema_editor):
-    if schema_editor.connection.vendor != "sqlite":
-        schema_editor.execute("ALTER TABLE core_workorder ALTER COLUMN kind DROP DEFAULT;")
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -19,21 +8,14 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.SeparateDatabaseAndState(
-            database_operations=[
-                migrations.RunPython(set_kind_defaults, unset_kind_defaults),
-            ],
-            state_operations=[
-                migrations.AddField(
-                    model_name="workorder",
-                    name="kind",
-                    field=models.CharField(
-                        choices=[("MAINTENANCE", "Maintenance"), ("MASS_ASSIGN", "Mass assignment")],
-                        db_index=True,
-                        default="MAINTENANCE",
-                        max_length=32,
-                    ),
-                ),
-            ],
+        migrations.AddField(
+            model_name="workorder",
+            name="kind",
+            field=models.CharField(
+                choices=[("MAINTENANCE", "Maintenance"), ("MASS_ASSIGN", "Mass assignment")],
+                db_index=True,
+                default="MAINTENANCE",
+                max_length=32,
+            ),
         ),
     ]
